2870ef711a0df999afca3f3d085372cfba6eb40e
suraj-bhavsar <suraj@questt.com>
2024-04-06 01:46:59 +0530

    ADD: Search song page

--- lib/app/modules/search_song_page/controllers/search_song_page_controller.dart
+++ lib/app/modules/search_song_page/controllers/search_song_page_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:music_player/app/data/repositories/songs_repository.dart';
+import 'package:music_player/app/utils/constants/enums.dart';
 
 class SearchSongPageController extends GetxController {
   final searchController = TextEditingController();
@@ -7,6 +9,10 @@ class SearchSongPageController extends GetxController {
   final _query = ''.obs;
   String get query => this._query.value;
   set query(String value) => this._query.value = value;
+
+  final _searchState = WidgetState.initial.obs;
+  WidgetState get searchState => this._searchState.value;
+  set searchState(WidgetState value) => this._searchState.value = value;
 
   final filteredSongs = [].obs;
 
@@ -31,12 +37,30 @@ class SearchSongPageController extends GetxController {
   }
 
   void _setupDebounce() {
-    debounce(_query, (_) => _getSearchResults());
+    debounce(
+      _query,
+      (_) => getSearchResults(),
+      time: const Duration(milliseconds: 500),
+    );
   }
 
   void onChangeQuery(String value) {
     query = value;
   }
 
-  void _getSearchResults() {}
+  void getSearchResults() async {
+    try {
+      if (query.trim().isEmpty) {
+        searchState = WidgetState.initial;
+        filteredSongs.clear();
+        return;
+      }
+      searchState = WidgetState.loading;
+      final searchedSongs = await SongsRepository.getSongsByQuery(query);
+      filteredSongs.assignAll(searchedSongs);
+      searchState = WidgetState.success;
+    } catch (e) {
+      searchState = WidgetState.error;
+    }
+  }
 }
--- lib/app/modules/search_song_page/views/search_song_page_view.dart
+++ lib/app/modules/search_song_page/views/search_song_page_view.dart
@@ -1,31 +1,102 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:music_player/app/utils/constants/enums.dart';
+import 'package:music_player/app/widgets/base_error_view.dart';
+import 'package:music_player/app/widgets/loader.dart';
+import 'package:music_player/app/widgets/song_card.dart';
 import '../controllers/search_song_page_controller.dart';
 
 class SearchSongPageView extends GetView<SearchSongPageController> {
   const SearchSongPageView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        titleSpacing: 0,
-        title: Padding(
-          padding: const EdgeInsets.only(right: 16),
-          child: CupertinoSearchTextField(
-            controller: controller.searchController,
-            onChanged: controller.onChangeQuery,
-            placeholder: 'Search songs',
-            style: Theme.of(context).textTheme.bodyLarge,
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          titleSpacing: 0,
+          title: Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: CupertinoSearchTextField(
+              controller: controller.searchController,
+              onChanged: controller.onChangeQuery,
+              placeholder: 'Search songs',
+              autofocus: true,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
           ),
         ),
+        body: const _Body(),
       ),
-      body: Center(
-        child: Text(
-          'SearchSongPageView is working',
-          style: TextStyle(fontSize: 20),
+    );
+  }
+}
+
+class _Body extends GetView<SearchSongPageController> {
+  const _Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      switch (controller.searchState) {
+        case WidgetState.initial:
+          return Container();
+        case WidgetState.loading:
+          return const LoaderWidget();
+        case WidgetState.error:
+          return BaseErrorView(onTryAgain: controller.getSearchResults);
+        case WidgetState.success:
+          if (controller.filteredSongs.isEmpty) {
+            return Center(child: const _EmptySearchResults());
+          } else {
+            return ListView.separated(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+              itemBuilder: (context, index) {
+                final song = controller.filteredSongs[index];
+                return SongCard(
+                  song: song,
+                  onTapFavourite: () {},
+                  // isFavourite: controller.isMarkedFavourite(song.id!),
+                  isFavourite: false,
+                );
+              },
+              separatorBuilder: (context, index) => Divider(
+                height: 10,
+                thickness: 1,
+                color: Theme.of(context).hintColor.withOpacity(0.05),
+              ),
+              itemCount: controller.filteredSongs.length,
+            );
+          }
+      }
+    });
+  }
+}
+
+class _EmptySearchResults extends StatelessWidget {
+  const _EmptySearchResults({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.search,
+          color: Theme.of(context).iconTheme.color,
+          size: 72,
         ),
-      ),
+        const SizedBox(height: 12),
+        Text(
+          'No songs found!',
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall
+              ?.copyWith(fontWeight: FontWeight.w500),
+        ),
+      ],
     );
   }
 }
